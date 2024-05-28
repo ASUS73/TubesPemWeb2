d766fcfca878f5fc792e8cc6e713cef0b06492f5
ASUS73 <114318674+ASUS73@users.noreply.github.com>
2024-05-28 15:06:16 +0700

    Add files via upload

---
+++ home/footer.php
@@ -0,0 +1,9 @@
+<div class="sticky-bottom">
+    <div class="container-fluid bg-dark bg-gradient pt-2">
+        <div class="row">
+            <div class="col-sm-12">
+                <p class="text-white text-center">&copy;Copyright 2024 | built by Ammar Yusron Abhinaya</p>
+            </div>
+        </div>
+    </div>
+</div>
